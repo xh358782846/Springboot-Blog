a542f015c2cf35fd467115af5634d884563655c2
xh358782846 <45600184+xh358782846@users.noreply.github.com>
2020-05-12 19:49:04 +0800

    Update UserServiceImpl.java

--- blog-idea/src/main/java/com/xhan/blog/service/impl/UserServiceImpl.java
+++ blog-idea/src/main/java/com/xhan/blog/service/impl/UserServiceImpl.java
@@ -13,6 +13,17 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private TUserMapper tUserMapper;
+
+    @Override
+    public int insert(TUser record) {
+
+        record.setPassword(MD5Utils.Code(record.getPassword()));
+        record.setCreateTime(new Date());
+        record.setType(1);
+        record.setAvatar("/images/logo.jpg");
+        return tUserMapper.insert(record);
+
+    }
     @Override
     public TUser checkUser(String username, String password) {
         TUser tUser=tUserMapper.findByUsernameAndPassword(username,MD5Utils.Code(password));

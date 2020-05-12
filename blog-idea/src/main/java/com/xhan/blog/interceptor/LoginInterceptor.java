b30eab6c8825daaa698a558a8e578d91f988374f
xh358782846 <45600184+xh358782846@users.noreply.github.com>
2020-05-12 19:47:03 +0800

    Update LoginInterceptor.java

--- blog-idea/src/main/java/com/xhan/blog/interceptor/LoginInterceptor.java
+++ blog-idea/src/main/java/com/xhan/blog/interceptor/LoginInterceptor.java
@@ -7,12 +7,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class LoginInterceptor extends HandlerInterceptorAdapter {
-
     @Override
     public boolean preHandle(HttpServletRequest request,
                              HttpServletResponse response,
                              Object handler) throws Exception {
-        if (request.getSession().getAttribute("user")==null){
+        if (request.getRequestURI().equals("/admin/register")){
+            return true;
+        }else if (request.getSession().getAttribute("user")==null){
+            System.out.println(request.getRequestURI());
             response.sendRedirect("/admin");
             return false;
         }

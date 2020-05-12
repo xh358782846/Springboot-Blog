ce1bbdd546fbdd0d4bbef49f8220b57f6afac0d5
xh358782846 <45600184+xh358782846@users.noreply.github.com>
2020-05-12 19:42:17 +0800

    Add files via upload
    修改注册

---
+++ blog-idea/src/main/java/com/xhan/blog/controller/LoginController.java
@@ -0,0 +1,68 @@
+package com.xhan.blog.controller.admin;
+
+
+import com.xhan.blog.dao.TUserMapper;
+import com.xhan.blog.pojo.TUser;
+import com.xhan.blog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpSession;
+
+
+@Controller
+@RequestMapping("/admin")
+public class LoginController {
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private TUserMapper tUserMapper;
+
+    @GetMapping(value = "toRegister")
+    public String registerPage(){
+        return "admin/register";
+    }
+
+    @PostMapping(value = "register")
+    public String register( TUser user,RedirectAttributes redirectAttributes) {
+        System.out.println("123");
+        if (tUserMapper.selectByName(user.getUsername())!=null) {
+            redirectAttributes.addFlashAttribute("message", "注册失败,用户名重复");
+            return "redirect:toRegister";
+        } else {
+            userService.insert(user);
+            return "admin/login";
+        }
+    }
+
+    @GetMapping
+    public String loginPage(){
+        return "admin/login";
+    }
+
+    @PostMapping(value = "login")
+    public String login(@RequestParam String username, String password,
+                        HttpSession session, RedirectAttributes redirectAttributes){
+        TUser tUser=userService.checkUser(username,password);
+        if (tUser!=null){
+            tUser.setPassword(null);
+            session.setAttribute("user",tUser);
+            return "admin/index";
+        }else {
+            redirectAttributes.addFlashAttribute("message","用户名和密码错误");
+            return "redirect:/admin";
+        }
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session){
+        session.removeAttribute("user");
+        return "redirect:/admin";
+    }
+}
